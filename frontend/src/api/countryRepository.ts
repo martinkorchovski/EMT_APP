0974bbe9319a0e2a5d1be489aa32a24c4edc286b
Martin Korchovski <martinkorchovski@gmail.com>
2026-05-16 12:00:36 +0200

    Remove repositories

--- frontend/src/api/countryRepository.ts
+++
@@ -1,8 +0,0 @@
-import axiosInstance from './axiosInstance';
-import type { Country } from '../types';
-
-export const getAllCountries = () =>
-    axiosInstance.get<Country[]>('/countries');
-
-export const getCountryById = (id: number) =>
-    axiosInstance.get<Country>(`/countries/${id}`);
